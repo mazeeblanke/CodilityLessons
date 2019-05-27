5c689f85478a7054464804955b4edcbc88292c18
Mazino Ukah <ewomaukah@yahoo.com>
2019-05-27 14:55:31 +0100

    Refactor missing integer solution

--- MissingInteger/index.js
+++ MissingInteger/index.js
@@ -1,19 +1,3 @@
-// Write a function:
-
-// function solution(A);
-
-// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-
-// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
-// Given A = [1, 2, 3], the function should return 4.
-
-// Given A = [−1, −3], the function should return 1.
-
-// Write an efficient algorithm for the following assumptions:
-
-// N is an integer within the range [1..100,000];
-// each element of array A is an integer within the range [−1,000,000..1,000,000].
 
 function solution(A) {
   const dictionary = {}
@@ -31,3 +15,5 @@ function solution(A) {
   return minimum;
 
 }
+
+module.exports = solution
